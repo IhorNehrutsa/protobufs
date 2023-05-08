3ace39e4c1f9d919dbde146d499e45779631f97e
Ihor Nehrutsa <Ihor.Nehrutsa@gmail.com>
2023-05-08 14:45:56 +0300

    Regenerate enum PtdButtonEvent
    PtdButtons {button_pin button_state buttons_states}
    
    Update deviceonly.proto
    
    Create .gitignore
    
    Update protoc.bat
    
    Add meshtastic_csharp_out files
    
    Add meshtastic_python_out files
    
    Update .gitignore
    
    Add enum PtdButtonEvent

--- protoc.bat
+++ protoc.bat
@@ -1,11 +1,12 @@
-set SRC_DIR=meshtastic
+set SRC_DIR=protobufs\meshtastic
 set DST_DIR=%SRC_DIR%
+
+cd ..
 
 if not exist %DST_DIR%_csharp_out  md %DST_DIR%_csharp_out
 if not exist %DST_DIR%_python_out  md %DST_DIR%_python_out
-if not exist %DST_DIR%_cpp_out     md %DST_DIR%_cpp_out
 
-for %%f in (%SRC_DIR%\*.proto) do protoc.exe -I=%SRC_DIR% --csharp_out=%DST_DIR%_csharp_out %%f
-for %%f in (%SRC_DIR%\*.proto) do protoc.exe -I=%SRC_DIR% --python_out=%DST_DIR%_python_out %%f
+for %%f in (%SRC_DIR%\*.proto) do protobufs\protoc.exe -I=protobufs --csharp_out=%DST_DIR%_csharp_out %%f
+for %%f in (%SRC_DIR%\*.proto) do protobufs\protoc.exe -I=protobufs --python_out=%DST_DIR%_python_out %%f
 
 pause
